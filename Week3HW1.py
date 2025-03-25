5e99738361e87f6cbec95d4234f9c001d2c3b705
kchhq <uim933@naver.com>
2025-03-25 12:49:01 +0900

    3주차 과제

---
+++ Week3HW1.py
@@ -0,0 +1,13 @@
+print("입력 진수 결정 (16/10/8/2) : ")
+a = int(input())
+
+print("값 입력 : ")
+b = input()
+
+print("\n16진수 ==> ", hex(int(b,a)))
+print("\n10진수 ==> ", int(b))
+print("\n8진수 ==> ", oct(int(b,a)))
+print("\n2진수 ==> ", bin(int(b,a)))
+
+
+
